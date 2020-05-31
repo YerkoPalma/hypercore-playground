5c92f10432776a40a551b77b9af4df61d5750b87
Yerko Palma <yerko.palma@usach.cl>
2020-05-31 00:55:23 -0400

    First attemp, broken chat
    Peers are connected, but there are issues
    
    1. The most notable issue is that messages are duplicated, probably
    repeated once per peer.
    2. When a peer diconnect it disconnect the whole chat.

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-function sum (a, b) {
-  return a + b
-}
-module.exports = sum
+/**
+ * run
+ * node . <name>
+ *
+ * This will join a channel with <name> or create one if it doen't exists
+ * and output any message from that channel. Also stdin will go to that channel,
+ * so it is basically a chat
+ */
+const hyperswarm = require('hyperswarm')
+const sodium = require('sodium-universal')
+
+const { crypto_generichash, crypto_generichash_BYTES } = sodium
+const swarm = hyperswarm()
+
+const name = process.argv[2]
+
+// create discovery key
+const key = Buffer.alloc(crypto_generichash_BYTES)
+crypto_generichash(key, Buffer.from(name))
+
+swarm.join(key, {
+  announce: true,
+  lookup: true
+})
+
+swarm.on('connection', (socket, info) => {
+  socket.pipe(process.stdout)
+  process.stdin.pipe(socket)
+  console.log('connection!')
+  console.log(info.peer)
+})
+
+process.on('exit', () => {
+  swarm.leave(key)
+})
